d1fd406ea7d1816b1e0a5269f38eacac14ce0aa7
turb <turbzor@gmail.com>
2023-08-21 17:46:24 +0300

    basic project structure/styling

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,23 @@
-const dataBtn = document.querySelector('.data-btn');
+window.addEventListener('DOMContentLoaded', (event) => {
+    const dataBtn = document.querySelector('.data-btn');
 
-dataBtn.addEventListener('click', (event) => {
+    dataBtn.addEventListener('click', (event) => {
+        fetch('http://localhost:3000/producers')
+            .then((data) => data.json())
+            .then((result) => {
+                for (res of result) {
+                    res.songsSources.forEach((item) => {
+                        dataBtn.insertAdjacentHTML('afterend', `<div>${item}</div>`);
+                    });
+                }
+            });
+    });
+
     fetch('http://localhost:3000/producers')
         .then((data) => data.json())
         .then((result) => {
             for (res of result) {
-                res.songsSources.forEach((item) => {
-                    dataBtn.insertAdjacentHTML('afterend', `<div>${item}</div>`);
-                });
+                dataBtn.insertAdjacentHTML('beforebegin', `${res.songsSources.length} <br>`);
             }
         });
 });
